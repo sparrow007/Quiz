4cec1a6fa336ff84f0bf11244b1cb0a97ee67943
SuperHaker <amrits3624@gmail.com>
2018-08-04 01:44:01 +0530

    Added nav drawer

--- app/src/main/java/android/zersey/expense_manger/Main2Activity.java
+++ app/src/main/java/android/zersey/expense_manger/Main2Activity.java
@@ -4,9 +4,14 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.PagerAdapter;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -46,14 +51,52 @@ public class Main2Activity extends AppCompatActivity implements Transactions.OnF
     private ViewGroup Transaction_linearLayout;
     private boolean Added=false;
     private ViewPager mViewPager;
+    private DrawerLayout mDrawerLayout;
+	private ActionBarDrawerToggle mDrawerToggle;
 
-    @Override
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+		ActionBar actionbar = getSupportActionBar();
+		if (actionbar != null) {
+			actionbar.setDisplayHomeAsUpEnabled(true);
+			actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+		}
+
+
+		mDrawerLayout = findViewById(R.id.drawer_layout);
+		mDrawerToggle =
+			new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.navigation_drawer_open,
+				R.string.navigation_drawer_close);
+		mDrawerLayout.addDrawerListener(mDrawerToggle);
+		mDrawerLayout.post(new Runnable() {
+			@Override
+			public void run() {
+				mDrawerToggle.syncState();
+			}
+		});
+
+		NavigationView navigationView = findViewById(R.id.nav_view);
+		navigationView.setNavigationItemSelectedListener(
+			new NavigationView.OnNavigationItemSelectedListener() {
+				@Override
+				public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+					// set item as selected to persist highlight
+					menuItem.setChecked(true);
+					// close drawer when item is tapped
+					mDrawerLayout.closeDrawers();
+
+					// Add code here to update the UI based on the item selected
+					// For example, swap UI fragments here
+
+					return true;
+				}
+			});
+
 
 
         // Create the adapter that will return a fragment for each of the three
@@ -143,17 +186,15 @@ public class Main2Activity extends AppCompatActivity implements Transactions.OnF
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+		switch (item.getItemId()) {
+			case R.id.action_settings:
+				return true;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+			case android.R.id.home:
+				mDrawerLayout.openDrawer(GravityCompat.START);
+				return true;
+		}
+		return super.onOptionsItemSelected(item);
     }
 
     @Override
