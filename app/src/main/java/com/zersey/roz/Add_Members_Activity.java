3ea3eb272dda5160b0ba42be1b71b9d536b13bc8
Bharat <S@bharat1>
2018-09-03 18:24:35 +0530

    Added the Search Field for contacts 3 Sep 2018 at 6:23 PM

--- app/src/main/java/com/zersey/roz/Add_Members_Activity.java
+++ app/src/main/java/com/zersey/roz/Add_Members_Activity.java
@@ -4,10 +4,16 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+
 import com.zersey.roz.Data.TransactionDbHelper;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +22,8 @@ public class Add_Members_Activity extends AppCompatActivity {
 	private List<ContactModel> Contact_List = new ArrayList<>();
 	public static Contact_RecyclerView_Adapter Add_Member_Adapter;
 	public static List<ContactModel> Added_Members;
+	public EditText Search_Edit;
+	private int count=0;
 	public static Contact_List_RecyclerView_Adapter Adapter;
 
 	@Override protected void onCreate(Bundle savedInstanceState) {
@@ -25,25 +33,53 @@ public class Add_Members_Activity extends AppCompatActivity {
 		setSupportActionBar(toolbar);
 		initContactList();
 		initRecyclerView();
+
+		Search_Edit.addTextChangedListener(new TextWatcher() {
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+			}
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Adapter.Search(s.toString());
+			}
+
+			@Override
+			public void afterTextChanged(Editable s) {
+                Adapter.Search(s.toString());
+			}
+		});
+
 	}
 
 	private void initContactList() {
 		TransactionDbHelper instance = TransactionDbHelper.getInstance(this);
 		Contact_List.addAll(instance.getContacts());
+		Search_Edit=findViewById(R.id.search_view);
+
+		for (int i=0;i<Contact_List.size();i++){
+			if (Contact_List.get(i).getUserId()!=0){
+             count++;
+			}
+		}
+		Log.d("initContactList: ",count+"");
 	}
 
 	public void initRecyclerView() {
 		Added_Members = new ArrayList<>();
 		RecyclerView contactListRecyclerView = findViewById(R.id.Contacts_List_RecyclerView);
 		contactListRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-		Adapter = new Contact_List_RecyclerView_Adapter(Contact_List);
+		Adapter = new Contact_List_RecyclerView_Adapter(Contact_List,count);
 		contactListRecyclerView.setAdapter(Adapter);
+
 
 		RecyclerView addedMembersRecyclerView = findViewById(R.id.ADD_Members_RecyclerView);
 		addedMembersRecyclerView.setLayoutManager(
 			new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
 		Add_Member_Adapter = new Contact_RecyclerView_Adapter(Added_Members);
 		addedMembersRecyclerView.setAdapter(Add_Member_Adapter);
+//		contactListRecyclerView.addItemDecoration(new DividerItemDecoration(getApplicationContext(), DividerItemDecoration.VERTICAL),1);
 	}
 
 	public void Add_Members(View view) {
--- app/src/main/java/com/zersey/roz/Contact_List_RecyclerView_Adapter.java
+++ app/src/main/java/com/zersey/roz/Contact_List_RecyclerView_Adapter.java
@@ -3,21 +3,28 @@ package com.zersey.roz;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Contact_List_RecyclerView_Adapter
 	extends RecyclerView.Adapter<Contact_List_RecyclerView_Adapter.ListViewHolder> {
-	private List<ContactModel> list;
+	private List<ContactModel> list,temp_list,Permanent_list;
 	private Add_Members_Activity Members;
 	private Context context;
+	private int Count;
 
-	public Contact_List_RecyclerView_Adapter(List<ContactModel> list) {
+	public Contact_List_RecyclerView_Adapter(List<ContactModel> list,int Count) {
 		this.list = list;
+		this.Permanent_list = list;
+		this.Count=Count;
 	}
 
 	@NonNull @Override
@@ -29,6 +36,10 @@ public class Contact_List_RecyclerView_Adapter
 	}
 
 	@Override public void onBindViewHolder(@NonNull ListViewHolder holder, final int position) {
+if (position==Count-1){
+if (Count!=0){
+holder.LineDivider.setVisibility(View.VISIBLE);}
+}
 		holder.Name.setText(list.get(position).getName());
 		holder.Number.setText(list.get(position).getNumber());
 
@@ -60,6 +71,32 @@ public class Contact_List_RecyclerView_Adapter
 		});
 	}
 
+	public void Search(String str){
+	    temp_list=new ArrayList<>();
+
+	    if(!TextUtils.isEmpty(str)) {
+			for (int i = 0; i < Permanent_list.size(); i++) {
+				if (Permanent_list.get(i).getName().toLowerCase().contains(str.toLowerCase())) {
+					temp_list.add(Permanent_list.get(i));
+				}
+			}
+
+			if(temp_list.size()>0){
+				list=temp_list;
+			}else {
+				list=new ArrayList<>();
+
+			}
+	    }else {
+	    	//list=new ArrayList<>();
+			list=Permanent_list;
+		}
+
+
+		notifyDataSetChanged();
+
+	}
+
 	@Override public int getItemCount() {
 		return list.size();
 	}
@@ -67,11 +104,13 @@ public class Contact_List_RecyclerView_Adapter
 	public class ListViewHolder extends RecyclerView.ViewHolder {
 
 		private TextView Name, Number;
+		private LinearLayout LineDivider;
 
 		public ListViewHolder(View itemView) {
 			super(itemView);
 			Name = itemView.findViewById(R.id.Contact_Name_TextView);
 			Number = itemView.findViewById(R.id.Contact_Number_TextView);
+			LineDivider=itemView.findViewById(R.id.Line_Divider);
 		}
 	}
 }
